8dcd64b5bc52b1791c885a27ed17d8bbec7ea70d
xs2445 <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>
2022-05-07 04:26:31 +0000

    q value

--- data/download_dataset.sh
+++ data/download_dataset.sh
@@ -3,7 +3,7 @@ set -e
 
 FILE_NAME=speech_commands_v0.01.tar.gz
 URL=http://download.tensorflow.org/data/$FILE_NAME
-DATASET_FOLDER=data/speech_commands_v1
+DATASET_FOLDER=speech_commands_v1
 
 echo "downloading $URL...\n"
 wget -O $DATASET_FOLDER/$FILE_NAME $URL
@@ -13,7 +13,7 @@ TEMP_DIRECTORY=$DATASET_FOLDER/audio
 mkdir -p $TEMP_DIRECTORY
 tar -xzf $DATASET_FOLDER/$FILE_NAME -C $TEMP_DIRECTORY
 
-echo "splitting the dataset into train, validation and test sets..."
-python $DATASET_FOLDER/split_dataset.py $DATASET_FOLDER
+# echo "splitting the dataset into train, validation and test sets..."
+# python $DATASET_FOLDER/split_dataset.py $DATASET_FOLDER
 
 echo "done
